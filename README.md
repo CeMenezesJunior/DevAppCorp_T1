740b4053d54658ef0a33afcb1897ebb72cebb297
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-07-03 13:19:43 -0300

    Projeto sem a nova funcionalidade

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# DevAppCorp_T1
+# Tarefa 1
+## O que foi feito
+### Escolha 
+
+Foi selecionada a utilização do Servlet HelloServlet.java, no projeto
+
+### Mudanças estéticas da página de index
+
+Na tela de índice foi alterada a posição do texto que foi para o centro,
+o fundo de tela foi mudado para preto e as letras para branco, o link foi descido
+e mudou o texto de link para clique aqui, a frase inicial agora está com em negrito
+para receber o devido destaque
+
+### Idiomas escolhidos
+
+Os dois novos idiomas adicionados foram o italiano e o espanhol
+
+### Alteração para bom dia 
+
+Foi criada uma método que compara a hora atual e diz se é manhã, tarde ou noite
+e com isso dá o bom dia, o boa tarde ou o boa noite dependendo do momento do dia
+
+### Nova funcionalidade
+
+A nova funcionalidade que foi adicionada é que quando o usuário recebe vai para tela de hello, 
+também recebe a hora do dia em horas e minutos 
+
+## Link
---
+++ src/main/java/hello/HelloServlet.java
@@ -0,0 +1,259 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author viter
+ */
+@WebServlet("/alomundo")
+public class HelloServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String msg = "";
+        
+        String lang = request.getParameter("lang");
+        if(lang==null)
+            lang = "pt";
+        switch(lang){
+            case "pt":
+                msg = "Alô, ";
+                break;
+            case "en":
+                msg = "Hello, ";
+                break;
+            case "fr":
+                msg = "Bonjour, ";
+                break;
+            case "it":
+                msg = "Ciao, ";
+                break;
+            case "es":
+                msg = "Hola, ";
+                break;
+        }
+        
+        String nome = request.getParameter("nome");
+
+        if(nome==null)
+            nome = "Fulano";
+        
+        msg = msg+nome+"!";
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloServlet</h1>");
+            out.println("<p>" + msg + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String msg = "";
+        int hora = TestaHora();
+        String lang = request.getParameter("lang");
+        if(lang==null)
+            lang = "pt";
+        switch(lang){
+            case "pt":
+                if(hora == 1){
+                    msg = "Bom dia, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Boa tarde, ";
+                    }
+                    else{
+                        msg = "Boa noite, ";
+                    }
+                }
+                break;
+            case "en":
+                if(hora == 1){
+                    msg = "Good morning, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Good afternoon, ";
+                    }
+                    else{
+                        msg = "Good evening, ";
+                    }
+                }
+                break;
+            case "fr":
+                if(hora == 1){
+                    msg = "Bonjour, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Bon après-midi, ";
+                    }
+                    else{
+                        msg = "Bonne nuit, ";
+                    }
+                }
+                break;
+            case "de":
+                if(hora == 1){
+                    msg = "Guten morgen, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Guten tag, ";
+                    }
+                    else{
+                        msg = "Gute nacht, ";
+                    }
+                }
+                break;
+            case "it":
+                 if(hora == 1){
+                    msg = "Buongiorno, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Buon pomeriggio, ";
+                    }
+                    else{
+                        msg = "Buona Notte, ";
+                    }
+                }
+                break;
+            case "es":
+                 if(hora == 1){
+                    msg = "Buenos dias, ";
+                }
+                else{
+                    if(hora == 2){
+                        msg = "Buenas tardes, ";
+                    }
+                    else{
+                        msg = "Buenas noches, ";
+                    }
+                }
+                break;
+        }
+        
+        String nome = request.getParameter("nome");
+
+        if(nome==null)
+            nome = "Fulano";
+        
+        msg = msg+nome+"!";
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloServlet</h1>");
+            out.println("<p>" + msg + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    private int TestaHora(){
+        LocalTime horaAgora = LocalTime.now();
+        LocalTime horaTarde = LocalTime.parse("12:00:00");
+        LocalTime horaNoite = LocalTime.parse("18:00:00");
+        int isManha = horaAgora.compareTo(horaTarde);
+        int isNoite = horaAgora.compareTo(horaNoite);
+        if(isNoite >= 0){
+            return 3;
+        }
+        else{
+            if(isManha < 0){
+                return 1;
+            }
+            else{
+                return 2;
+            }
+            
+        }
+    }
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
